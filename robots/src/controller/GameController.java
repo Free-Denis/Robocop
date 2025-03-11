02bbe0243d83ade429e6016c5e48c5a3f1a12a89
Free-Denis <147907574+Free-Denis@users.noreply.github.com>
2025-03-11 10:50:58 +0500

    Add files via upload

--- robots/src/controller/GameController.java
+++ robots/src/controller/GameController.java
@@ -1,17 +1,32 @@
 package controller;
 
-import model.RobotModel;
+import model.Model;
 import view.GameVisualizer;
 
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class GameController {
-    private final RobotModel robotModel;
+    private final Model model;
     private final GameVisualizer gameVisualizer;
 
     public GameController() {
-        robotModel = new RobotModel();
-        gameVisualizer = new GameVisualizer(robotModel);
+        model = new Model(); // Model теперь реализует EntityProvider
+        gameVisualizer = new GameVisualizer(model); // Передаем Model как EntityProvider
+
+        Timer timer = new Timer(10, e -> {
+            model.update(10);
+            gameVisualizer.repaint();
+        });
+        timer.start();
+
+        gameVisualizer.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                model.sendMouseClickEvent(e.getX(), e.getY());
+            }
+        });
     }
 
     public void start() {
--- robots/src/view/GameVisualizer.java
+++ robots/src/view/GameVisualizer.java
@@ -1,32 +1,22 @@
 package view;
 
-import model.RobotModel;
+import model.Entity;
+import model.EntityProvider;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.geom.AffineTransform;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class GameVisualizer extends JPanel {
-    private final RobotModel robotModel;
-    private final Timer timer;
+    private final EntityProvider entityProvider;
+    private final Map<Class<? extends Entity>, EntityVisualizer<?>> visualizers = new HashMap<>();
 
-    public GameVisualizer(RobotModel robotModel) {
-
-        this.robotModel = robotModel;
-        timer = new Timer(10, e -> {
-            robotModel.update(10);
-            repaint();
-        });
-        timer.start();
-
-        addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                robotModel.setTargetPosition(e.getX(), e.getY());
-            }
-        });
+    public GameVisualizer(EntityProvider entityProvider) {
+        this.entityProvider = entityProvider;
+        // Регистрируем визуализатор для робота
+        visualizers.put(model.Robot.class, new RobotVisualizer());
     }
 
     @Override
@@ -34,38 +24,19 @@ public class GameVisualizer extends JPanel {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
 
-        drawRobot(g2d, (int) robotModel.getRobotPositionX(), (int) robotModel.getRobotPositionY(), robotModel.getRobotDirection());
-        drawTarget(g2d, robotModel.getTargetPositionX(), robotModel.getTargetPositionY());
-
+        List<Entity> entities = entityProvider.getEntities();
+        for (Entity entity : entities) {
+            EntityVisualizer<?> visualizer = visualizers.get(entity.getClass());
+            if (visualizer != null) {
+                // Используем cast для корректного типа
+                drawEntity(g2d, entity, visualizer);
+            }
+        }
     }
 
-    private void drawRobot(Graphics2D g, int x, int y, double direction) {
-        AffineTransform t = AffineTransform.getRotateInstance(direction, x, y);
-        g.setTransform(t);
-        g.setColor(Color.MAGENTA);
-        fillOval(g, x, y, 30, 10);
-        g.setColor(Color.BLACK);
-        drawOval(g, x, y, 30, 10);
-        g.setColor(Color.WHITE);
-        fillOval(g, x + 10, y, 5, 5);
-        g.setColor(Color.BLACK);
-        drawOval(g, x + 10, y, 5, 5);
-    }
-
-    private void drawTarget(Graphics2D g, int x, int y) {
-        AffineTransform t = AffineTransform.getRotateInstance(0, 0, 0);
-        g.setTransform(t);
-        g.setColor(Color.GREEN);
-        fillOval(g, x, y, 5, 5);
-        g.setColor(Color.BLACK);
-        drawOval(g, x, y, 5, 5);
-    }
-
-    private static void fillOval(Graphics g, int centerX, int centerY, int diam1, int diam2) {
-        g.fillOval(centerX - diam1 / 2, centerY - diam2 / 2, diam1, diam2);
-    }
-
-    private static void drawOval(Graphics g, int centerX, int centerY, int diam1, int diam2) {
-        g.drawOval(centerX - diam1 / 2, centerY - diam2 / 2, diam1, diam2);
+    private <E extends Entity> void drawEntity(Graphics2D g, E entity, EntityVisualizer<?> visualizer) {
+        @SuppressWarnings("unchecked")
+        EntityVisualizer<E> typedVisualizer = (EntityVisualizer<E>) visualizer;
+        typedVisualizer.draw(g, entity);
     }
 }
