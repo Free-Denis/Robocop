c144898c1f7124bd51363896d29d4a7f090122c7
Free-Denis <147907574+Free-Denis@users.noreply.github.com>
2025-03-11 10:50:14 +0500

    Delete src directory

--- src/view/EntityVisualizer.java
+++
@@ -1,8 +0,0 @@
-package view;
-
-import model.Entity;
-import java.awt.Graphics2D;
-
-public abstract class EntityVisualizer<E extends Entity> {
-    public abstract void draw(Graphics2D g, E entity);
-}
